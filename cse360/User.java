62a57951ab47d186bcf09e111e8ee200a49cfaf7
ethicalzeus07 <128852337+ethicalzeus07@users.noreply.github.com>
2024-10-10 12:09:50 +0530

    Update User.java

--- cse360/User.java
+++ cse360/User.java
@@ -1,36 +1,60 @@
-
 package cse360;
 
 import java.io.Serializable;
 
-public abstract class User implements Serializable {
-    protected String firstName;
-    protected String middleName;
-    protected String lastName;
-    protected String username;
-    protected String passwordHash;
-    protected String email;
+public class User implements Serializable {
+    private static final long serialVersionUID = 542148557600126242L;
 
-    public User(String firstName, String middleName, String lastName, String username, String passwordHash, String email) {
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String email;
+    private String username;
+    private String password;
+    private String role;
+
+    // Constructor
+    public User(String firstName, String middleName, String lastName, String email, String username, String password, String role) {
         this.firstName = firstName;
         this.middleName = middleName;
         this.lastName = lastName;
-        this.username = username;
-        this.passwordHash = passwordHash;
         this.email = email;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    // Getters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
     }
 
     public String getUsername() {
         return username;
     }
 
-    public String getPasswordHash() {
-        return passwordHash;
+    public String getPassword() {
+        return password;
     }
 
-    public String getFullName() {
-        return firstName + " " + middleName + " " + lastName;
+    public String getRole() {
+        return role;
     }
 
-    public abstract String getRole();
+    @Override
+    public String toString() {
+        return firstName + " " + middleName + " " + lastName + " (" + role + ")";
+    }
 }
